2bea00ea52d685563b25cb93608d6cb0e0dc4bd3
Alexey Gulyaev <ghost-shadows@yandex.ru>
2021-09-16 13:00:31 +0300

    Initial Commit

---
+++ UIKit_App_Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_App_Lesson2
+//
+//  Created by Алексей Гуляев on 16.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
